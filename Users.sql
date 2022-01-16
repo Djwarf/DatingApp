778807a779a799df4663926ce3b6743d76805efc
djwarf <djwar.fettah@outlook.com>
2022-01-16 20:26:19 +0000

    End of section 2

---
+++ Users.sql
@@ -0,0 +1,21 @@
+﻿-- Script Date: 16/01/2022 19:40  - ErikEJ.SqlCeScripting version 3.5.2.90
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (<Id, bigint,>
+           ,<1, "Dj",>);
+           
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (<Id, bigint,>
+           ,<2, "Bahez",>);
+           
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (<Id, bigint,>
+           ,<3, "Yad",>);
